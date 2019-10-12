b1e01623137dffc5e484aae6770b404c828475a3
Siltanen <smsiltan@LM8-516-005.local>
2019-10-12 18:04:06 +0300

    naive reconstructions revised

--- deconv03_naive_comp.m
+++ deconv03_naive_comp.m
@@ -2,8 +2,18 @@
 %
 % Samuli Siltanen Sep 2019
 
+% Choose signal 1 or 2
+sig_num = 1;
+
 % Load precomputed stuff
-load data/deconv02 n xvec Dx f tvec p pn mn
+load data/deconv02 n xvec Dx tvec p pn f1 m1 mn1 f2 m2 mn2
+if sig_num==1
+    f = f1;
+    mn = mn1;
+else
+    f = f2;
+    mn = mn2;
+end
 
 % Let us build the measurement matrix using the ideal PSF
 A = DC_convmtx(p,n);
@@ -16,7 +26,7 @@ recIC  = inv(A)*mIC(:);
 recn   = inv(A)*mn(:);
 
 % Save results to disc
-save data/deconv03 A recIC recn mIC
+save data/deconv03 A recIC recn mIC sig_num
 
 deconv03_naive_plot
 
--- deconv03_naive_plot.m
+++ deconv03_naive_plot.m
@@ -11,8 +11,17 @@ colorGray = [.5 .5 .5];
 colorRecon= [144 2 190]/255;
 
 % Load precomputed stuff
-load data/deconv02 n xvec f 
-load data/deconv03 mIC recIC recn
+load data/deconv02 n xvec Dx tvec p pn f1 m1 mn1 f2 m2 mn2
+load data/deconv03 A recIC recn mIC sig_num
+if sig_num==1
+    f = f1;
+    mn = mn1;
+else
+    f = f2;
+    mn = mn2;
+end
+ymin = min(f)-.2;
+ymax = max(f)+.2;
 
 % Calculate reconstruction errors
 errIC = norm(recIC(:)-f(:))/norm(f(:));
@@ -30,18 +39,18 @@ hold on
 plot(xvec,mIC,'b','linewidth',lwidth)
 set(gca,'xticklabel','','fontsize',fsize)
 set(gca,'ytick',[0,1],'fontsize',fsize)
-axis([0 1 -.2 1.6])
+axis([0 1 ymin ymax])
 title('Data WITH inverse crime','fontsize',fsize)
 
 % Plot reconstruction
 subplot(2,1,2)
-p1 = plot(xvec,f,'k','linewidth',lwidth)
+p1 = plot(xvec,f,'k','linewidth',lwidth);
 set(p1,'color',colorGray)
 hold on
 p2 = plot(xvec,recIC,'b','linewidth',lwidth);
 set(p2,'color',colorRecon)
 set(gca,'ytick',[0,1],'fontsize',fsize)
-axis([0 1 -.2 1.6])
+axis([0 1 ymin ymax])
 title(['Naive reconstruction WITH inverse crime, error ',num2str(100*errIC,'%0.1f'),'%'],'fontsize',fsize)
 
 
@@ -52,25 +61,25 @@ clf
 
 % Plot target and noisy measurement
 subplot(2,1,1)
-p1 = plot(xvec,f,'k','linewidth',lwidth)
+p1 = plot(xvec,f,'k','linewidth',lwidth);
 set(p1,'color',colorGray)
 hold on
 plot(xvec,mn,'r','linewidth',lwidth)
 set(gca,'xticklabel','','fontsize',fsize)
 set(gca,'ytick',[0,1],'fontsize',fsize)
-axis([0 1 -.2 1.6])
+axis([0 1 ymin ymax])
 title('Noisy measurement','fontsize',fsize)
 
 
 % Plot reconstruction
 subplot(2,1,2)
-p1 = plot(xvec,f,'k','linewidth',lwidth)
+p1 = plot(xvec,f,'k','linewidth',lwidth);
 set(p1,'color',colorGray)
 hold on
 p2 = plot(xvec,recn,'b','linewidth',lwidth);
 set(p2,'color',colorRecon)
 set(gca,'ytick',[0,1],'fontsize',fsize)
-axis([0 1 -.2 1.6])
+axis([0 1 ymin ymax])
 title(['Naive reconstruction, no inverse crime, error ',num2str(100*errn,'%0.1f'),'%'],'fontsize',fsize)
 
 
