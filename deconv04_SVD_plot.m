c103fc7eec30eeaa840980afd0f27c7a0de21a7b
Siltanen <smsiltan@staff-189-159.vpn.helsinki.fi>
2019-10-12 14:00:56 +0300

    Updated the SVD computations and illutrations

--- deconv04_SVD_plot.m
+++ deconv04_SVD_plot.m
@@ -1,14 +1,16 @@
-% Plot the results of deconv4_SVD_comp.m
+% Plot the results of deconv4_SVD_comp.m. Show the singular values of the
+% system matrix. 
 %
-% Samuli Siltanen January 2015
+% Samuli Siltanen Oct 2019
 
 % Parameters for controlling the plot
 lwidth = 1;
-fsize  = 20;
+fsize  = 16;
 msize  = 6;
 
 % Load precomputed stuff
 load data/SVD A U D V svals
+[row,col] = size(A);
 
 % Create a plot window
 figure(1)
@@ -16,9 +18,11 @@ clf
 
 % Plot singular values
 semilogy([1:length(svals)],svals,'b.','markersize',msize)
+set(gca,'xtick',[1 round(col/4) round(col/2) round(3*col/4) col],'fontsize',fsize)
 axis square
 xlim([1 length(svals)])
- 
+title('Singular values of the convolution matrix, log scale','fontsize',fsize)
+
 % % Create a plot window
 % figure(2)
 % clf
